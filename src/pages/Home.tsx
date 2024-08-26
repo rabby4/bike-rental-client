73c1d3cf5761bd8c4fe56dbfdbee2ed58591fd3e
Rabby <mdrabbybd4747@gmail.com>
2024-08-26 15:36:50 +0600

    create feature bike section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import Carousel from "@/components/home/Carousel"
+import FeaturedBikes from "@/components/home/FeaturedBikes"
 
 const Home = () => {
 	return (
 		<>
 			<Carousel />
+			<FeaturedBikes />
 		</>
 	)
 }
